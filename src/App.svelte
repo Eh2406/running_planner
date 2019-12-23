a6318d086b97831c57a531ecd22d2eeb48c00750
Finkelman <finkelman@semcog.org>
2019-12-22 21:59:07 -0500

    better layout

--- src/App.svelte
+++ src/App.svelte
@@ -84,7 +84,8 @@
     display: inline-block;
   }
   .form {
-    display: inline-block;
+    display: inline-grid;
+    grid-template-columns: repeat(2, 1fr);
   }
   .report {
     display: inline-block;
@@ -98,69 +99,83 @@
 <h1>Let's plan a run! :)</h1>
 <div class="all">
   <div class="form">
-    <label>Reps:</label>
-    <input
-      type="number"
-      bind:value={reps}
-      name="reps"
-      min="1"
-      max="10"
-      step="1" />
-
-    <label>Warmup-time:</label>
-    <input
-      type="number"
-      bind:value={utime}
-      name="utime"
-      min="1"
-      max="10"
-      step="0.25" />
-    min
-    <label>Warmup-speed:</label>
-    <input
-      type="number"
-      bind:value={uspeed}
-      name="uspeed"
-      min="1"
-      max="7"
-      step="0.1" />
-    mph
-    <label>Walk-time:</label>
-    <input
-      type="number"
-      bind:value={wtime}
-      name="wtime"
-      min="1"
-      max="10"
-      step="0.25" />
-    min
-    <label>Walk-speed:</label>
-    <input
-      type="number"
-      bind:value={wspeed}
-      name="wspeed"
-      min="1"
-      max="7"
-      step="0.1" />
-    mph
-    <label>Run-time:</label>
-    <input
-      type="number"
-      bind:value={rtime}
-      name="rtime"
-      min="1"
-      max="10"
-      step="0.25" />
-    min
-    <label>Run-speed:</label>
-    <input
-      type="number"
-      bind:value={rspeed}
-      name="rspeed"
-      min="1"
-      max="7"
-      step="0.1" />
-    mph
+    <div>
+      <label>Reps:</label>
+      <input
+        type="number"
+        bind:value={reps}
+        name="reps"
+        min="1"
+        max="10"
+        step="1" />
+    </div>
+    <div />
+    <div>
+      <label>Warmup-speed:</label>
+      <input
+        type="number"
+        bind:value={uspeed}
+        name="uspeed"
+        min="1"
+        max="7"
+        step="0.1" />
+      mph
+    </div>
+    <div>
+      <label>Warmup-time:</label>
+      <input
+        type="number"
+        bind:value={utime}
+        name="utime"
+        min="1"
+        max="10"
+        step="0.25" />
+      min
+    </div>
+    <div>
+      <label>Walk-speed:</label>
+      <input
+        type="number"
+        bind:value={wspeed}
+        name="wspeed"
+        min="1"
+        max="7"
+        step="0.1" />
+      mph
+    </div>
+    <div>
+      <label>Walk-time:</label>
+      <input
+        type="number"
+        bind:value={wtime}
+        name="wtime"
+        min="1"
+        max="10"
+        step="0.25" />
+      min
+    </div>
+    <div>
+      <label>Run-speed:</label>
+      <input
+        type="number"
+        bind:value={rspeed}
+        name="rspeed"
+        min="1"
+        max="7"
+        step="0.1" />
+      mph
+    </div>
+    <div>
+      <label>Run-time:</label>
+      <input
+        type="number"
+        bind:value={rtime}
+        name="rtime"
+        min="1"
+        max="10"
+        step="0.25" />
+      min
+    </div>
   </div>
   <div class="report">
     <h2>This will take {ttime} min</h2>
