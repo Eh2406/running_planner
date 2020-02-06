c86944c5aa4e9e6880dc746dfa9284ab9bc8a155
Eh2406 <YeomanYaacov@gmail.com>
2020-02-05 20:42:31 -0500

    remove expand mode

--- src/Runs.svelte
+++ src/Runs.svelte
@@ -14,8 +14,6 @@
 
   let willChange = "d";
 
-  let expanded = true;
-
   let udist = 0;
   let rdist = 0;
   let wdist = 0;
@@ -139,12 +137,6 @@
   <div class="controls">
     <button
       on:click={() => {
-        expanded = !expanded;
-      }}>
-      {#if expanded}-{:else}+{/if}
-    </button>
-    <button
-      on:click={() => {
         dispatch('duplicate');
       }}>
       duplicate
@@ -156,107 +148,100 @@
       x
     </button>
   </div>
-  {#if expanded}
-    <table>
-      <tr>
-        <th>Reps:</th>
+  <table>
+    <tr>
+      <th>Reps:</th>
+      <td>
+        <input
+          type="number"
+          bind:value={reps}
+          name="reps"
+          min="1"
+          max="10"
+          step="1" />
+      </td>
+    </tr>
+    <tr>
+      <th />
+      <th
+        on:click={_ => {
+          willChange = 's';
+        }}>
+        Speed (mph)
+      </th>
+      <th
+        on:click={_ => {
+          willChange = 't';
+        }}>
+        Time (min)
+      </th>
+      <th
+        on:click={_ => {
+          willChange = 'd';
+        }}>
+        Dist (mile)
+      </th>
+    </tr>
+    <SpeedTime
+      preaf="Warmup"
+      bind:speed={uspeed}
+      bind:time={utime}
+      bind:dist={udist}
+      {willChange} />
+    <SpeedTime
+      preaf="Walk"
+      bind:speed={wspeed}
+      bind:time={wtime}
+      bind:dist={wdist}
+      {willChange} />
+    <SpeedTime
+      preaf="Run"
+      bind:speed={rspeed}
+      bind:time={rtime}
+      bind:dist={rdist}
+      {willChange} />
+  </table>
+  <div class="report">
+    <h2>This will take {ttime.toFixed(2)} min</h2>
+    <div class="row-cart">
+      {#each arrayOfIntervals as interval}
+        <div
+          class={interval.type}
+          style="width: {100 * (interval.time / ttime)}%;">
+          &nbsp;
+        </div>
+      {/each}
+    </div>
+    <h2>
+      and will go {tdist.toFixed(2)} miles / {(tdist * 1.60934).toFixed(2)} km
+    </h2>
+    <div class="row-cart">
+      {#each arrayOfIntervals as interval}
+        <div
+          class={interval.type}
+          style="width: {100 * (interval.dist / tdist)}%;">
+          &nbsp;
+        </div>
+      {/each}
+    </div>
+  </div>
+  <table>
+    {#each arrayOfIntervals as interval, i}
+      <tr
+        class={interval.type}
+        class:resets={!!resets[i]}
+        class:need-resets={interval.timeSoFar > 60}
+        on:click={() => {
+          resets[i] = !resets[i];
+        }}>
+        <th>{i + 1}</th>
+        <td>{{ warmup: '🧘', run: '🏃', walk: '🚶' }[interval.type] || ''}</td>
+        <td>{interval.time.toFixed(2)} @ {interval.speed.toFixed(2)} mph</td>
         <td>
-          <input
-            type="number"
-            bind:value={reps}
-            name="reps"
-            min="1"
-            max="10"
-            step="1" />
+          Stoping at {interval.distSoFar.toFixed(2)} mile | {interval.timeSoFar.toFixed(2)}
+          min
         </td>
       </tr>
-      <tr>
-        <th />
-        <th
-          on:click={_ => {
-            willChange = 's';
-          }}>
-          Speed (mph)
-        </th>
-        <th
-          on:click={_ => {
-            willChange = 't';
-          }}>
-          Time (min)
-        </th>
-        <th
-          on:click={_ => {
-            willChange = 'd';
-          }}>
-          Dist (mile)
-        </th>
-      </tr>
-      <SpeedTime
-        preaf="Warmup"
-        bind:speed={uspeed}
-        bind:time={utime}
-        bind:dist={udist}
-        {willChange} />
-      <SpeedTime
-        preaf="Walk"
-        bind:speed={wspeed}
-        bind:time={wtime}
-        bind:dist={wdist}
-        {willChange} />
-      <SpeedTime
-        preaf="Run"
-        bind:speed={rspeed}
-        bind:time={rtime}
-        bind:dist={rdist}
-        {willChange} />
-    </table>
-    <div class="report">
-      <h2>This will take {ttime.toFixed(2)} min</h2>
-      <div class="row-cart">
-        {#each arrayOfIntervals as interval}
-          <div
-            class={interval.type}
-            style="width: {100 * (interval.time / ttime)}%;">
-            &nbsp;
-          </div>
-        {/each}
-      </div>
-      <h2>
-        and will go {tdist.toFixed(2)} miles / {(tdist * 1.60934).toFixed(2)} km
-      </h2>
-      <div class="row-cart">
-        {#each arrayOfIntervals as interval}
-          <div
-            class={interval.type}
-            style="width: {100 * (interval.dist / tdist)}%;">
-            &nbsp;
-          </div>
-        {/each}
-      </div>
-    </div>
-    <table>
-      {#each arrayOfIntervals as interval, i}
-        <tr
-          class={interval.type}
-          class:resets={!!resets[i]}
-          class:need-resets={interval.timeSoFar > 60}
-          on:click={() => {
-            resets[i] = !resets[i];
-          }}>
-          <th>{i + 1}</th>
-          <td>
-            {{ warmup: '🧘', run: '🏃', walk: '🚶' }[interval.type] || ''}
-          </td>
-          <td>{interval.time.toFixed(2)} @ {interval.speed.toFixed(2)} mph</td>
-          <td>
-            Stoping at {interval.distSoFar.toFixed(2)} mile | {interval.timeSoFar.toFixed(2)}
-            min
-          </td>
-        </tr>
-      {/each}
-    </table>
-  {:else}
-    <div>Run {reps} x {rtime.toFixed(2)} min @ {rspeed.toFixed(2)} mph</div>
-    <div>Totaling {tdist.toFixed(2)} miles in {ttime.toFixed(2)} min</div>
-  {/if}
+    {/each}
+  </table>
 </div>
